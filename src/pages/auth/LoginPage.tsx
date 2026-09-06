9686450b5f851cc154e3ddc9c84472816a083ac5
mr-taiss <taiskamilah@gmail.com>
2026-09-06 22:31:12 +0200

    Mostrar mensaje de nombre no registrado

--- src/pages/auth/LoginPage.tsx
+++ src/pages/auth/LoginPage.tsx
@@ -1,50 +1,36 @@
 import { useState } from "react";
 import { Navigate, useNavigate } from "react-router-dom";
 
-
 import LoginForm from "../../components/auth/LoginForm";
 import { authRepository } from "../../repositories/authRepository";
-
-
 import type { LoginCredentials } from "../../types/auth";
-
 
 function LoginPage() {
   const navigate = useNavigate();
   const [error, setError] = useState("");
 
-
   if (authRepository.isAuthenticated()) {
     return <Navigate to="/" replace />;
   }
 
-
   const handleLogin = (credentials: LoginCredentials) => {
     setError("");
 
-
     const user = authRepository.login(credentials);
 
-
     if (!user) {
-      setError("El carnet o la contraseña son incorrectos.");
+      setError("Nombre no registrado");
       return;
     }
-
 
     navigate("/", { replace: true });
   };
 
-
   return (
     <main>
-      <LoginForm
-        error={error}
-        onSubmit={handleLogin}
-      />
+      <LoginForm error={error} onSubmit={handleLogin} />
     </main>
   );
 }
-
 
 export default LoginPage;
