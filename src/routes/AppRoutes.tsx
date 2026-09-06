7355940e6f28b831b8bc747fda8f3e51f473ed25
mr-taiss <taiskamilah@gmail.com>
2026-09-06 23:00:39 +0200

    fix: protect home route with authorized login

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,24 +1,32 @@
 import {
   BrowserRouter,
+  Navigate,
   Route,
   Routes,
 } from "react-router-dom";
 
-
 import HomePage from "../pages/HomePage";
 import LoginPage from "../pages/auth/LoginPage";
+import { authRepository } from "../repositories/authRepository";
 
+function ProtectedHome() {
+  if (!authRepository.isAuthenticated()) {
+    return <Navigate to="/login" replace />;
+  }
+
+  return <HomePage />;
+}
 
 function AppRoutes() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/" element={<ProtectedHome />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
   );
 }
-
 
 export default AppRoutes;
