8d35637f04fec12e1c81d1f8443a26fb0a537b2f
mr-taiss <taiskamilah@gmail.com>
2026-09-06 22:30:57 +0200

    Actualizar credenciales para acceso por nombre

--- src/types/auth.ts
+++ src/types/auth.ts
@@ -1,6 +1,5 @@
 export type UserRole = "ADMIN" | "USUARIO";
 
-
 export interface User {
   id: string;
   name: string;
@@ -8,13 +7,10 @@ export interface User {
   role: UserRole;
 }
 
-
 export interface UserRecord extends User {
   password: string;
 }
 
-
 export interface LoginCredentials {
-  carnet: string;
-  password: string;
+  name: string;
 }
